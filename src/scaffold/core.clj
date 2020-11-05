9b27584d29cb9ed8fc3593bc275a31dc619dd47a
Børge André Jensen <borgeajensen@gmail.com>
2020-11-06 00:21:35 +0100

    cleaned up core ns

--- src/scaffold/core.clj
+++ src/scaffold/core.clj
@@ -1,11 +1,2 @@
 (ns scaffold.core
   (:require [clojure.string :as str]))
-
-(def users {:table  "users"
-            :columns [["id" :uuid [:pk [:default "uuid_v4_generate"] :not-null]]
-                      ["username" :citext [:unique [:check "LEN(username) > 3"] :not-null]]
-                      ["password" :text [:not-null]]]})
-
-(def profile {:table   "profile"
-              :columns [["user_id" :uuid [[:fk "users (id)"] :not-null]]
-                        ["avatar_url" :url [:not-null]]]})
