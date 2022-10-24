2a5a22e6a2892dc35853a782c24b488cb72b32b0
Børge André Jensen <borgeajensen@gmail.com>
2022-10-24 15:25:04 +0200

    preparation for scaffolding

--- src/scaffold/core.clj
+++ src/scaffold/core.clj
@@ -1,42 +1,64 @@
 (ns scaffold.core
   (:require [clojure.string :as str]
             [scaffold.postgres.core :as pg]
-            [scaffold.postgres.query :as q]))
+            [scaffold.postgres.query :as q]
+            [scaffold.reitit :as reitit]
+            [scaffold.model :as m]))
 
 (defn generate-migration
-  ([table-spec]
-   (generate-migration table-spec (str "add-" (:name table-spec) "-table.up.sql")))
-  ([table-spec filename]
-   (spit filename (pg/table-sql table-spec))))
+  [table-spec]
+  (pg/table-sql table-spec))
 
 (defn generate-hugsql-queries
-  ([table-spec]
-   (generate-hugsql-queries table-spec {}))
-  ([table-spec {:keys [filename]
-                :or   {filename (str (:name table-spec) "-queries.sql")}
-                :as   opts}]
-   (let [insert-sql
-         (str (q/hugsql-signature table-spec :insert {:depluralize? true})
-              (q/insert table-spec (comp q/append-column-cast q/hugsql-var)))
-         
-         select-sql
-         (str (q/hugsql-signature table-spec :select {:depluralize? true})
-              (q/select table-spec))
+  [table-spec]
+  (let [insert-sql
+        (str (q/hugsql-signature table-spec :insert {:depluralize? true})
+             (q/insert table-spec (comp q/append-column-cast q/hugsql-var)))
 
-         select-by-pk-sql
-         (str (q/hugsql-signature table-spec :select-by-pk {:depluralize? true})
-              (q/select table-spec))
-         
-         update-sql
-         (str (q/hugsql-signature table-spec :update {:depluralize? true})
-              (q/update table-spec (comp q/append-column-cast q/hugsql-var)))
+        select-sql
+        (str (q/hugsql-signature table-spec :select {:depluralize? true})
+             (q/select table-spec))
 
-         delete-sql
-         (str (q/hugsql-signature table-spec :delete {:depluralize? true})
-              (q/delete table-spec (comp q/append-column-cast q/hugsql-var)))]
-     (spit filename (str/join "\n\n" [insert-sql select-sql select-by-pk-sql update-sql delete-sql])))))
+        select-by-pk-sql
+        (str (q/hugsql-signature table-spec :select-by-pk {:depluralize? true})
+             (q/select table-spec))
 
+        update-sql
+        (str (q/hugsql-signature table-spec :update {:depluralize? true})
+             (q/update table-spec (comp q/append-column-cast q/hugsql-var)))
+
+        delete-sql
+        (str (q/hugsql-signature table-spec :delete {:depluralize? true})
+             (q/delete table-spec (comp q/append-column-cast q/hugsql-var)))]
+    (str/join "\n\n" [insert-sql select-sql select-by-pk-sql update-sql delete-sql])))
 
 (def sample-table-spec
   {:name "users"
-   :columns [["id" [:uuid] [[:primary-key]]]]})
+   :columns [["id" [:uuid] [[:primary-key]]]
+             ["email" [:citext]]]})
+
+(def sample-configuration
+  {:migrations/dir         "resources/migrations/"
+   :migrations/overwrite?  false
+   :reitit/insert?         true
+   :reitit/routes-file     "" ;; file containing routes
+   :reitit/routes-var      'routes ;; var containing routes vector
+   :reitit/insert-path     [:__last :__after]
+   ;; values:
+   #_                      [[:__first :__before]
+                           [:__first :__after]
+                           [:__last :__before]
+                           ["/users" :__after]
+                           ["/users" :__before]]
+   :hugsql/queries-dir     "resources/queries/"
+   :hugsql/queries-file    "my-queries.sql"
+   :hugsql/queries-append? false
+   :hugsql/query-fn        'db.my-query-fn})
+
+(defn scaffold [configuration table-spec]
+  (let [migration (generate-migration table-spec)
+        queries (generate-hugsql-queries table-spec)
+        routes (reitit/routes table-spec (:hugsql/query-fn configuration))]
+    {:migration migration
+     :queries queries
+     :routes routes}))
